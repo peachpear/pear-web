3fdee73ff5dd9db642ac4454b22dab4061c0b196
zhengjunyuan <zhengjunyuan@peilian.com>
2020-04-02 16:43:58 +0800

    components edit2

--- common/config/dev.php
+++ common/config/dev.php
@@ -3,13 +3,13 @@ defined("ENV") || define("ENV", "dev");
 $baseConfig = include('base.php');
 
 $commonConfig = array(
-	'components' => [
-		'cache' => [
+    'components' => [
+        'cache' => [
             'host' => 'test.redis.demo.com',
             'port' => 6379,
             'keyPrefix' => 'demoDev.',
-		],
-        'demoDB' => [
+        ],
+        'demoDB'  => [
             'dsn' => 'mysql:host=192.168.100.2;port=3306;dbname=demo',
             'username' => 'demo',
             'password' => 'demo_+-*123',
